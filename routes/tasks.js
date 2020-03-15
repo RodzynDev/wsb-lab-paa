423b0bca8a5c254d5e011978942925c8dd915805
Dawid Tomas <dawidt@dev-effectivity.pl>
2020-03-15 21:04:39 +0100

    Checking if title field is empty then showing error

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,8 +4,14 @@ const store = require('../store')
 router.prefix('/tasks')
 
 router.post('/add', async (ctx, next) => {
-  await store.createTask(ctx.request.body.title, ctx.request.body.desc)
-  ctx.redirect('/')
+  if(ctx.request.body.title !== "") {
+    await store.createTask(ctx.request.body.title, ctx.request.body.desc)
+    ctx.redirect('/')
+  } else {
+    await ctx.render('errorMessage', {
+      message: "Tytuł zadania nie może być pusty"
+    })
+  }
 })
 
 module.exports = router
